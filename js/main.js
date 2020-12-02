6dd7d1fb3f057dc21e89b5c10e2df7656d3976c2
ShristiJalan <shristijalan1010@gmail.com>
2020-12-02 23:45:46 +0530

    automatic scroll

--- js/main.js
+++ js/main.js
@@ -9,7 +9,12 @@ $(document).ready(function(){
     })
 
 // owl-carousel for blog
-$('.owl-carousel').owlCarousel();
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false
+});
 
 
 });
