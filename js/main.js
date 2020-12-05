2687bb196b5e2164ca5267ebcfc4e74215354f29
ShristiJalan <shristijalan1010@gmail.com>
2020-12-06 03:12:56 +0530

    scroll feature added

--- js/main.js
+++ js/main.js
@@ -19,6 +19,11 @@ $('.owl-carousel').owlCarousel({
     // responsive: responsive
 });
 
-
+//click to scroll top
+$('.move-up span').click(function(){
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000);
+})
 });
 
